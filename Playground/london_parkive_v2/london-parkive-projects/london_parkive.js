4afa5fec1323b13c48ae3896415b8f3873f49ea7
smallslope <mayaosaka@outlook.com>
2024-03-13 10:56:31 +0000

    Addition of nav bar. Styled buttons and pop ups

--- Playground/london_parkive_v2/london-parkive-projects/london_parkive.js
+++ Playground/london_parkive_v2/london-parkive-projects/london_parkive.js
@@ -7,6 +7,8 @@ var map = new maplibregl.Map({
 });
 var parksInformation = false;
 var parksShapes = false;
+var southwarkParksShapes = false;
+
 var popup = new maplibregl.Popup({closeOnClick: false});
 //Status Polygon Colours//
 var statusOpenColor = "#50b848";
@@ -18,19 +20,19 @@ var statusUnderThreatColor = "#fee400";
 document.getElementById("status_under_threat").style = `background-color: ${statusUnderThreatColor}`;
 document.getElementById("alterations_under_threat").style = `background-color: ${statusUnderThreatColor}`
 //Year Opened Polygon Colours//
-var yoBefore1850Color = "#870E8A";
+var yoBefore1850Color = "#FA2DFF";
 document.getElementById("yo_before1850").style = `background-color: ${yoBefore1850Color}`;
-var yo1850to1874Color = "#CA1CCD";
+var yo1850to1874Color = "#FD70FF";
 document.getElementById("yo_1850to1874").style = `background-color: ${yo1850to1874Color}`;
-var yo1875to1899Color = "#FC51FF";
+var yo1875to1899Color = "#FEC7FF";
 document.getElementById("yo_1875to1899").style = `background-color: ${yo1875to1899Color}`;
-var yo1900to1924Color = "#102C8F";
+var yo1900to1924Color = "#00A3FF";
 document.getElementById("yo_1900to1924").style = `background-color: ${yo1900to1924Color}`;
-var yo1925to1949Color = "#244BD6";
+var yo1925to1949Color = "#45BBFD";
 document.getElementById("yo_1925to1949").style = `background-color: ${yo1925to1949Color}`;
-var yo1950to1974Color = "#809CFF";
+var yo1950to1974Color = "#A2DEFF";
 document.getElementById("yo_1950to1974").style = `background-color: ${yo1950to1974Color}`;
-var yo1975to1999Color = "#BDCCFF";
+var yo1975to1999Color = "#D1EEFF";
 document.getElementById("yo_1975to1999").style = `background-color: ${yo1975to1999Color}`;
 var yo2000to2024Color = "#AAA30D";
 document.getElementById("yo_2000to2024").style = `background-color: ${yo2000to2024Color}`;
@@ -107,7 +109,7 @@ var alterationParksLayer = {
     paint : {
         "fill-color" : [
             "match",
-            ["get", "altered"],
+            ["get", "alteration"],
             "Unchanged", statusOpenColor,
             "Expanded", alterationsExpandedColor,
             "Shrank", statusOpenColor,
@@ -130,37 +132,36 @@ var expandedParksLayer = {
     filter : ["==", ["get", "alteration"], "Expanded"]
 } 
 
-mergeParkData = function (parkinfo, parkshapes){
-    console.log(parkinfo);
-    console.log(parkshapes);
-    parkshapes.features.forEach((feature) => {
-    console.log(feature.properties);
-    let data = parkinfo.filter((info)=>info.name == feature.properties.name);
-    console.log(data);
-    feature.properties = data[0]
-    });
-    console.log(parkshapes);
-}
+// mergeParkData = function (parkinfo, parkshapes){
+//     console.log(parkinfo);
+//     console.log(parkshapes);
+//     parkshapes.features.forEach((feature) => {
+//     console.log(feature.properties);
+//     let data = parkinfo.filter((info)=>info.name == feature.properties.name);
+//     console.log(data);
+//     feature.properties = data[0]
+//     });
+//     console.log(parkshapes);
+// }
+
 map.on('load', () =>{
 
-    fetch('./data_files/london_parks_info.json')
-    .then(response => response.json())
-    .then(response => {
-    parksInformation = response;
-    console.log(parksInformation);
-    })
+    // fetch('./data_files/london_parks_info.json')
+    // .then(response => response.json())
+    // .then(response => {
+    //     parksInformation = response;
+    //     console.log(parksInformation);
+    // })
     fetch('./data_files/park_shapes_source.json')
     .then(response => response.json())
     .then(response => {
     parksShapes = response;
     console.log(parksShapes);
     })
-
     map.addSource("parkShapesSource",{
         type : "geojson",
         data : './data_files/park_shapes_source.json'
     })
-
     map.addSource("boroughs_shapes",{
         type : "geojson",
         data : './data_files/uk_local_district_boundaries.json'            
@@ -188,26 +189,134 @@ map.on('load', () =>{
     map.addLayer(underThreatParksLayer);
 })
 
+//Functions tyhat assign colours depending on different values.//
 
-map.on('click', 'park_status_layer', function(e){
+function setOpeningDateColor(clickedParkPeriod){
+    let returnColor;
+    switch(clickedParkPeriod){
+        case "<1850":
+            returnColor = yoBefore1850Color;
+            break;
+        case "1850-1874":
+            returnColor = yo1850to1874Color;
+            break;
+        case "1875-1899":
+            returnColor = yo1875to1899Color;
+            break;
+        case "1900-1924":
+            returnColor = yo1900to1924Color;
+            break;
+        case "1925-1949":
+            returnColor = yo1925to1949Color;
+            break;
+        case "1950-1974":
+            returnColor = yo1950to1974Color;
+            break;
+        case "1975-1999":
+            returnColor = yo1975to1999Color;
+            break;
+        case "2000-2024":
+            returnColor = yo2000to2024Color;
+            break;
+        default:
+            returnColor = "#000000";
+    }
+    return returnColor;
+}
+function setStatusColor(clickedParkStatus){
+    let returnColor;
+    switch(clickedParkStatus){
+        case "Open":
+            returnColor = statusOpenColor;
+            break;
+        case "Under Threat":
+            returnColor = statusUnderThreatColor;
+            break;
+        case "Defunct":
+            returnColor = statusDefunctColor;
+            break;
+        default:
+            returnColor = "#000000";
+    }
+    return returnColor;
+}
+function setAlterationColor(clickedParkAlteration){
+    let returnColor;
+    switch(clickedParkAlteration){
+        case "Unchanged":
+            returnColor = statusOpenColor;
+            break;
+        case "Under Threat":
+            returnColor = statusUnderThreatColor;
+            break;
+        case "Expanded":
+            returnColor = alterationsExpandedColor;
+            break;
+        case "Shrank":
+            returnColor = statusDefunctColor;
+            break;
+        default:
+            returnColor = "#000000";
+    }
+    return returnColor;
+}
+
+//PopUps//
+
+map.on('click', 'park_status_layer', (e) => {
     const clickedFeatures = map.queryRenderedFeatures(e.point, { layers : ['park_status_layer']})
     console.log(clickedFeatures)
 
     let clickedParkName = clickedFeatures[0].properties.name;
-    let yearOpened = 0;
-    parksInformation.forEach(function(thisPark){
-        if(thisPark.name === clickedParkName){
-            yearOpened = thisPark["year_opened"];
-            
-        } 
-    })
-    popup.setLngLat(e.lngLat).setHTML(
-        `${clickedParkName} was opened in ${yearOpened}`
-    ).addTo(map)
+    let clickedParkLongitude = clickedFeatures[0].properties.longitude;
+    let clickedParkLatitude = clickedFeatures[0].properties.latitude;
+    let clickedParkOpeningPeriod = clickedFeatures[0].properties.period_opened;
+    let clickedParkStatus = clickedFeatures[0].properties.status;
+    let clickedParkAlterations = clickedFeatures[0].properties.alteration;
+    console.log(clickedParkAlterations);
 
-    mergeParkData(parksInformation,parksShapes);
+    let openingPeriodColor = setOpeningDateColor(clickedParkOpeningPeriod);
+    let statusColor = setStatusColor(clickedParkStatus);
+    let alterationsColor = setAlterationColor(clickedParkAlterations);
+
+    popup.setLngLat(e.lngLat).setHTML(
+        `
+        <div class="popup_container">
+            <h3 class="popup_park_name">${clickedParkName}</h3>
+            <p class="popup_coordinates">${clickedParkLatitude}, ${clickedParkLongitude}</p>
+            <div class="popup_info_section">
+                <div class="popup_info_row">
+                    <p class="popup_info_type">Opened:</p>
+                    <div class="popup_info_box popup_opening_period" style="background-color:${openingPeriodColor}">
+                        <p class="popup_info_text">${clickedParkOpeningPeriod}</p>
+                    </div>
+                </div>
+                <div class="popup_info_row">
+                    <p class="popup_info_type">Status:</p>
+                    <div class="popup_info_box pop_up_status" style="background-color:${statusColor}">
+                        <p class="popup_info_text">${clickedParkStatus}</p>
+                    </div>
+                </div>
+                <div class="popup_info_row">
+                    <p class="popup_info_type">Alterations: </p>
+                    <div class="popup_info_box popup_alterations" style="background-color:${alterationsColor}">
+                        <p class="popup_info_text">${clickedParkAlterations}</p>
+                    </div>
+                </div>
+            </div>
+            <button class="tell_me_more_button">Tell Me More</button>
+        </div>
+        
+        `
+    ).addTo(map)
 })
+
+//View Mode Buttons//
+
 document.getElementById("status_button").addEventListener("click", function(){
+    document.getElementById("year_opened_button").classList.remove("view_mode_active");
+    document.getElementById("status_button").classList.add("view_mode_active");
+    document.getElementById("alterations_button").classList.remove("view_mode_active");
     map.setPaintProperty("year_opened_layer", "fill-opacity", 0)
     map.setPaintProperty("park_status_layer", "fill-opacity", 0.8)
     map.setPaintProperty("shrunk_park_areas", "fill-opacity", 0)
@@ -220,6 +329,9 @@ document.getElementById("status_button").addEventListener("click", function(){
     document.getElementById("alterations_keys").style.display = "none";
 });
 document.getElementById("year_opened_button").addEventListener("click", function(){
+    document.getElementById("status_button").classList.remove("view_mode_active");
+    document.getElementById("year_opened_button").classList.add("view_mode_active");
+    document.getElementById("alterations_button").classList.remove("view_mode_active");
     map.setPaintProperty("year_opened_layer", "fill-opacity", 0.8)
     map.setPaintProperty("park_status_layer", "fill-opacity", 0)
     map.setPaintProperty("shrunk_park_areas", "fill-opacity", 0)
@@ -232,6 +344,9 @@ document.getElementById("year_opened_button").addEventListener("click", function
     document.getElementById("alterations_keys").style.display = "none";
 });
 document.getElementById("alterations_button").addEventListener("click", function(){
+    document.getElementById("status_button").classList.remove("view_mode_active");
+    document.getElementById("alterations_button").classList.add("view_mode_active");
+    document.getElementById("year_opened_button").classList.remove("view_mode_active");
     map.setPaintProperty("year_opened_layer", "fill-opacity", 0)
     map.setPaintProperty("park_status_layer", "fill-opacity", 0)
     map.setPaintProperty("shrunk_park_areas", "fill-opacity", 0.8)
